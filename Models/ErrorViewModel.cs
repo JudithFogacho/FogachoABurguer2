9e5d55cadc9187db5cf527880f8d4656f4905c48
laboratorio <laboratorio@UPEAULA-31722>
2024-10-21 10:10:29 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FogachoABurguer2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
